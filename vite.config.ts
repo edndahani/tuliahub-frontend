7b5f7d9ceb4f5cdc5684b266be20cea8fd742a2e
Emmanuel Ndahani <ndahani3d.en@gmail.com>
2026-04-04 21:59:14 +0300

    vite config

---
+++ vite.config.ts
@@ -0,0 +1,38 @@
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
+import svgr from "vite-plugin-svgr";
+import EnvironmentalPlugin from "vite-plugin-environment";
+
+const env = loadEnv(process.env.NODE_ENV, process.cwd(), "");
+
+const API_URL = env.VITE_API_URL;
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss(), svgr(), EnvironmentalPlugin("all")],
+  server: {
+    host: true,
+    proxy: {
+      "/api": {
+        target: API_URL,
+        changeOrigin: true,
+        configure: () => {},
+      },
+      "^/fallback/.*": {
+        target: API_URL,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/fallback/, ""),
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  optimizeDeps: {
+    exclude: ["@tanstack/react-table"],
+  },
+});
